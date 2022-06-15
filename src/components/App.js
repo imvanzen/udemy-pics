d322cc802ba03d8d5d05dab28f35e41e5d1db79c
J.Reczko <reczko.jakub@gmail.com>
2022-06-15 23:58:17 +0200

    Update formating in app.js

--- src/components/App.js
+++ src/components/App.js
@@ -3,9 +3,11 @@ import React from 'react';
 import SearchBar from './SearchBar';
 
 const App = () => {
-    return (<div className='app'>
-        <SearchBar />
-    </div>)
+    return (
+        <div className='app'>
+            <SearchBar />
+        </div>
+    )
 }
 
 export default App;
