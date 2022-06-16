4c632930724c3ce75f94c2736b1f92c3b14d13b0
J.Reczko <reczko.jakub@gmail.com>
2022-06-16 19:35:51 +0200

    refactor app component to the class component

--- src/components/App.js
+++ src/components/App.js
@@ -2,12 +2,14 @@ import React from 'react';
 
 import SearchBar from './SearchBar';
 
-const App = () => {
-    return (
-        <div className='app ui container' style={{marginTop: '10px'}}>
-            <SearchBar />
-        </div>
-    )
+class App extends React.Component {
+    render () {
+        return (
+            <div className='app ui container' style={{marginTop: '10px'}}>
+                <SearchBar />
+            </div>
+        )
+    }
 }
 
 export default App;
