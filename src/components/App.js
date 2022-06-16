5fa4c338b92323d2de511435791e493d94f1b47c
J.Reczko <reczko.jakub@gmail.com>
2022-06-16 20:27:46 +0200

    Set images to the state

--- src/components/App.js
+++ src/components/App.js
@@ -2,8 +2,9 @@ import React from 'react';
 import axios from 'axios';
 import SearchBar from './SearchBar';
 
-
 class App extends React.Component {
+    state = { images: [] }; 
+
     async onSearchSubmit(term) {
         const response = await axios.get('https://api.unsplash.com/search/photos', {
             params: { query: term },
@@ -12,7 +13,7 @@ class App extends React.Component {
             }
         });
 
-        console.log(response.data.results);
+        this.setState({images: response.data.results})
     }
 
     render () {
