2cf21260dbdd5991dd55d0c155f2f07ff2cbeba1
J.Reczko <reczko.jakub@gmail.com>
2022-06-16 19:44:11 +0200

    onvoce callback in the child component

--- src/components/App.js
+++ src/components/App.js
@@ -3,10 +3,14 @@ import React from 'react';
 import SearchBar from './SearchBar';
 
 class App extends React.Component {
+    onSearchSubmit = (term) => {
+        console.log(term);
+    }
+
     render () {
         return (
             <div className='app ui container' style={{marginTop: '10px'}}>
-                <SearchBar />
+                <SearchBar onSubmit={this.onSearchSubmit} />
             </div>
         )
     }
--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -7,7 +7,7 @@ class SearchBar extends React.Component {
     onFormSubmit = (e) => {
         e.preventDefault();
 
-        console.log(this.state.term)
+        this.props.onSubmit(this.state.term);
     }
 
     render() {
