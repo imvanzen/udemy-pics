734fe2fe6ca61a2a650bdb68a6040ba8f9482000
J.Reczko <reczko.jakub@gmail.com>
2022-06-17 22:45:44 +0200

    Use imagecard component and destruct necessary props

--- src/components/ImageList.js
+++ src/components/ImageList.js
@@ -1,10 +1,11 @@
 import './ImageList.css';
 
 import React from 'react';
+import ImageCard from './ImageCard'
 
 const ImageList = (props) => {
-    const images = props.images.map(({description, id, urls}) => (
-        <img alt={description} key={id} src={urls.regular} />
+    const images = props.images.map(image => (
+        <ImageCard image={image} />
     ));
  
     return (
