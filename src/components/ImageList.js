f92a86d4a1773926125a9aa4a4b11b1e480ab3d7
J.Reczko <reczko.jakub@gmail.com>
2022-06-17 21:27:37 +0200

    Map images from props to the list

--- src/components/ImageList.js
+++ src/components/ImageList.js
@@ -1,8 +1,14 @@
 import React from 'react';
 
-const ImageList = () => {
+const ImageList = (props) => {
+    const images = props.images.map(image => (
+        <img src={image.urls.regular} key={image.id} />
+    ));
+ 
     return (
-        <div className='image-list'></div>
+        <div className='image-list'>
+            {images}
+        </div>
     )
 }
 
