595d8a22325c5433ce02abd8f2b7817a1dc27d01
J.Reczko <reczko.jakub@gmail.com>
2022-06-17 21:44:33 +0200

    Add grid styles to the images list

--- src/components/ImageList.js
+++ src/components/ImageList.js
@@ -1,8 +1,10 @@
+import './ImageList.css';
+
 import React from 'react';
 
 const ImageList = (props) => {
-    const images = props.images.map(image => (
-        <img alt={image.description} src={image.urls.regular} key={image.id} />
+    const images = props.images.map(({description, id, urls}) => (
+        <img alt={description} key={id} src={urls.regular} />
     ));
  
     return (
