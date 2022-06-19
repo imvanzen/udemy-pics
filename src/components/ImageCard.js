986d7a96372da519782e4774ebd413d7e31f71bf
J.Reczko <reczko.jakub@gmail.com>
2022-06-19 10:31:08 +0200

    Create react reference to get image height

--- src/components/ImageCard.js
+++ src/components/ImageCard.js
@@ -1,11 +1,21 @@
-import React from "react";
+import React, { useRef } from "react";
 
 class ImageCard extends React.Component {
+    constructor (props) {
+        super(props);
+        this.imageRef = React.createRef();
+    }
+
+    componentDidMount() {
+        console.log(this.imageRef.current.clientHeight)
+    }
+
     render () {
         const {description, urls} = this.props.image;
 
         return (
             <img 
+                ref={this.imageRef}
                 alt={description} 
                 src={urls.regular}
             />
