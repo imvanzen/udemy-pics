bca23e6679a52eee75ef025affa27e8865907a40
J.Reczko <reczko.jakub@gmail.com>
2022-06-19 10:48:37 +0200

    init span state

--- src/components/ImageCard.js
+++ src/components/ImageCard.js
@@ -3,6 +3,7 @@ import React from "react";
 class ImageCard extends React.Component {
     constructor (props) {
         super(props);
+        this.state = {spans: 0};
         this.imageRef = React.createRef();
     }
 
@@ -11,6 +12,9 @@ class ImageCard extends React.Component {
     }
 
     setSpans = (event) => {
+        const height = this.imageRef.current.clientHeight
+        const spans = Math.ceil(height / 150);
+        this.setState({ spans });
     }
 
     render () {
